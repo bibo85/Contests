6b80c0ebf04a30e1f0d354856a63e8c28cef3278
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-04-14 14:11:29 +0300

    Add quick sort

---
+++ Разное/Сортировки/quick_sort.py
@@ -0,0 +1,21 @@
+# быстрая сортировка
+
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot, less, greater = supp_func(arr)
+
+    return quicksort(less) + pivot + quicksort(greater)
+
+
+def supp_func(arr):
+    pivot = [num for num in arr if num == arr[-1]]
+    less = [num for num in arr if num < arr[-1]]
+    greater = [num for num in arr if num > arr[-1]]
+    print(pivot, less, greater)
+    return pivot, less, greater
+
+
+numbers = [5, 8, 9, 4, 2, 9, 1, 8]
+print(quicksort(numbers))
