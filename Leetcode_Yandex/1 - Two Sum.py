06d38c4c83e3df6cb284cf9791eb49fef34e0184
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-04-15 23:42:17 +0300

    Task has been solved

---
+++ Leetcode_Yandex/1 - Two Sum.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # числа, которые мы видели и их индексы: {число:индекс}
+        seen = {}
+
+        for i, val in enumerate(nums):
+            # вычисляем, сколько нам не хватает, чтобы получить target
+            remaining = target - val
+
+            # если нужный остаток есть в уже просмотренных, то нашли ответ
+            if remaining in seen:
+                return [i, seen[remaining]]
+            else:
+                seen[val] = i
